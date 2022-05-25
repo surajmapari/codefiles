2e339a20540638726287ce147a939cccec166077
SURAJ MAPARI <57449206+surajmapari@users.noreply.github.com>
2022-05-26 01:58:46 +0530

    Update sequence-probability.cpp

--- dsa/sequence-probability.cpp
+++ dsa/sequence-probability.cpp
@@ -1,3 +1,9 @@
+/*
+Given sequence k = k1 <k2 < ... <kn of n sorted keys, with a search probability pi for each
+key ki . Build the Binary search tree that has the least search cost given the access
+probability for each key?
+*/
+
 #include <iostream>
 using namespace std;
 void con_obst(void);
