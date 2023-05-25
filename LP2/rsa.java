49d21dbfa4e6feba183ef4c7774f64586b5c6119
SURAJ MAPARI <57449206+surajmapari@users.noreply.github.com>
2023-05-25 08:16:22 +0530

    Add files via upload

---
+++ LP2/rsa.java
@@ -0,0 +1,39 @@
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.util.Base64;
+import javax.crypto.Cipher;
+
+public class rsa {
+    public static void main(String[] args) {
+        try {
+            // Generate RSA key pair
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048, new SecureRandom());
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+            PublicKey publicKey = keyPair.getPublic();
+            PrivateKey privateKey = keyPair.getPrivate();
+
+            String plainText = "Hello, World!";
+
+            // Encrypt the plaintext
+            Cipher encryptCipher = Cipher.getInstance("RSA");
+            encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] encryptedBytes = encryptCipher.doFinal(plainText.getBytes());
+            String encryptedText = Base64.getEncoder().encodeToString(encryptedBytes);
+            System.out.println("Encrypted Text: " + encryptedText);
+
+            // Decrypt the ciphertext
+            Cipher decryptCipher = Cipher.getInstance("RSA");
+            decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] decryptedBytes = decryptCipher.doFinal(Base64.getDecoder().decode(encryptedText));
+            String decryptedText = new String(decryptedBytes);
+            System.out.println("Decrypted Text: " + decryptedText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
