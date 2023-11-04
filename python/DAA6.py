b1eaec94d444e48385d2526f3f1a05d15089c95a
SURAJ MAPARI <57449206+surajmapari@users.noreply.github.com>
2023-11-05 00:03:12 +0530

    Add files via upload

---
+++ python/DAA6.py
@@ -0,0 +1,78 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+sample_input = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(sample_input)
+print("Sorted array:", sample_input)
+
+
+import threading
+
+def parallel_merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_thread = threading.Thread(target=parallel_merge_sort, args=(left_half,))
+    right_thread = threading.Thread(target=parallel_merge_sort, args=(right_half,))
+
+    left_thread.start()
+    right_thread.start()
+
+    left_thread.join()
+    right_thread.join()
+
+    i = j = k = 0
+
+    while i < len(left_half) and j < len(right_half):
+        if left_half[i] < right_half[j]:
+            arr[k] = left_half[i]
+            i += 1
+        else:
+            arr[k] = right_half[j]
+            j += 1
+        k += 1
+
+    while i < len(left_half):
+        arr[k] = left_half[i]
+        i += 1
+        k += 1
+
+    while j < len(right_half):
+        arr[k] = right_half[j]
+        j += 1
+        k += 1
+
+sample_input = [38, 27, 43, 3, 9, 82, 10]
+parallel_merge_sort(sample_input)
+print("Sorted array:", sample_input)
